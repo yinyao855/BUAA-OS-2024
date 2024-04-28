9b103b18da2751fe5293546aa65f42022bb7c8dd
尹耀 <21241072@buaa.edu.cn>
2024-04-28 20:29:53 +0800

    完成lab4所有练习

--- user/lib/fork.c
+++ user/lib/fork.c
@@ -21,22 +21,31 @@ static void __attribute__((noreturn)) cow_entry(struct Trapframe *tf) {
 	/* Hint: Use 'vpt' and 'VPN' to find the page table entry. If the 'perm' doesn't have
 	 * 'PTE_COW', launch a 'user_panic'. */
 	/* Exercise 4.13: Your code here. (1/6) */
+	perm = vpt[VPN(va)] & 0xfff;
+	if (!(perm & PTE_COW)) {
+		user_panic("cow_entry: page is not copy-on-write");
+	}
 
 	/* Step 2: Remove 'PTE_COW' from the 'perm', and add 'PTE_D' to it. */
 	/* Exercise 4.13: Your code here. (2/6) */
+	perm = (perm & ~PTE_COW) | PTE_D;
 
 	/* Step 3: Allocate a new page at 'UCOW'. */
 	/* Exercise 4.13: Your code here. (3/6) */
+	syscall_mem_alloc(0, (void *)va, perm);
 
 	/* Step 4: Copy the content of the faulting page at 'va' to 'UCOW'. */
 	/* Hint: 'va' may not be aligned to a page! */
 	/* Exercise 4.13: Your code here. (4/6) */
+	memcpy((void *)UCOW, (void *)ROUNDDOWN(va, PAGE_SIZE), PAGE_SIZE);
 
 	// Step 5: Map the page at 'UCOW' to 'va' with the new 'perm'.
 	/* Exercise 4.13: Your code here. (5/6) */
+	syscall_mem_map(0, (void *)UCOW, 0, (void *)va, perm);
 
 	// Step 6: Unmap the page at 'UCOW'.
 	/* Exercise 4.13: Your code here. (6/6) */
+	syscall_mem_unmap(0, (void *)UCOW);
 
 	// Step 7: Return to the faulting routine.
 	int r = syscall_set_trapframe(0, tf);
@@ -72,13 +81,24 @@ static void duppage(u_int envid, u_int vpn) {
 	/* Step 1: Get the permission of the page. */
 	/* Hint: Use 'vpt' to find the page table entry. */
 	/* Exercise 4.10: Your code here. (1/2) */
+	addr = vpn * PAGE_SIZE;
+	perm = vpt[vpn] & 0xfff;	// 获取权限位
 
 	/* Step 2: If the page is writable, and not shared with children, and not marked as COW yet,
 	 * then map it as copy-on-write, both in the parent (0) and the child (envid). */
 	/* Hint: The page should be first mapped to the child before remapped in the parent. (Why?)
 	 */
 	/* Exercise 4.10: Your code here. (2/2) */
-
+	int flag = 0;
+	if ((perm & PTE_D) && !(perm & PTE_LIBRARY)) {
+		perm &= ~PTE_D;
+		perm |= PTE_COW;
+		flag = 1;
+	} // 更改权限为写时复制
+	syscall_mem_map(0, addr, envid, addr, perm);	// 将父进程的页映射到子进程
+	if (flag){
+		syscall_mem_map(0, addr, 0, addr, perm);	
+	}
 }
 
 /* Overview:
@@ -113,6 +133,11 @@ int fork(void) {
 	/* Step 3: Map all mapped pages below 'USTACKTOP' into the child's address space. */
 	// Hint: You should use 'duppage'.
 	/* Exercise 4.15: Your code here. (1/2) */
+	for (i = 0; i < VPN(USTACKTOP); i++) {
+		if ((vpd[i >> 10] & PTE_V) && (vpt[i] & PTE_V)) {
+			duppage(child, i);
+		} // 复制父进程的页表，一级页表和二级页表都有效
+	}
 
 	/* Step 4: Set up the child's tlb mod handler and set child's 'env_status' to
 	 * 'ENV_RUNNABLE'. */
@@ -121,6 +146,9 @@ int fork(void) {
 	 *   Child's TLB Mod user exception entry should handle COW, so set it to 'cow_entry'
 	 */
 	/* Exercise 4.15: Your code here. (2/2) */
+	try(syscall_set_tlb_mod_entry(child, cow_entry));
+	try(syscall_set_env_status(child, ENV_RUNNABLE));
 
 	return child;
 }
+
