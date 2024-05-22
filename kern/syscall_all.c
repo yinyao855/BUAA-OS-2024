27e58e1cfbfb112b4ca6a32a60710b19ef88a623
尹耀 <21241072@buaa.edu.cn>
2024-05-22 21:29:07 +0800

    6 try

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -464,7 +464,7 @@ int sys_clone(void *func, void *child_stack){
 	e->env_tf = *((struct Trapframe *)KSTACKTOP - 1);
 
 	e->env_tf.cp0_epc = func;
-	e->env_tf.regs[0] = child_stack;
+	e->env_tf.regs[29] = child_stack;
 
 	e->env_status = ENV_RUNNABLE;
 	TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
