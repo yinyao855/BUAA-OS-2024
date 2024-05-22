896c697cb644fdc321a35826fce03bd75eb97d3e
尹耀 <21241072@buaa.edu.cn>
2024-05-22 11:37:43 +0000

    1 try

--- user/lib/strace.c
+++ user/lib/strace.c
@@ -17,8 +17,27 @@ void strace_send(int sysno) {
 	}
 
 	// Your code here. (1/2)
+	if (straced == 0){
+		return;
+	}
+	straced = 0;
+	syscall_ipc_try_send(env->env_parent_id, sysno, 0, 0);
+	syscall_set_env_status(env->env_id, ENV_NOT_RUNNABLE);
+	straced = 1;
 }
 
 void strace_recv() {
 	// Your code here. (2/2)
+	int child_id = 0;
+	int sysno = -1;
+	while(sysno = ipc_recv(&child_id, 0, 0)){
+		if (sysno != -1) {
+			strace_barrier(child_id);
+			recv_sysno(child_id, sysno);
+			syscall_set_env_status(child_id, ENV_RUNNABLE);
+		}
+		if (sysno == SYS_env_destroy){
+			break;
+		}
+	}
 }
