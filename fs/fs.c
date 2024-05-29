efd29014f1d77d08456100b83a834628f4303e57
尹耀 <21241072@buaa.edu.cn>
2024-05-29 12:42:08 +0000

    2 try

--- fs/fs.c
+++ fs/fs.c
@@ -501,6 +501,10 @@ int dir_lookup(struct File *dir, char *name, struct File **file) {
 	/* Exercise 5.8: Your code here. (1/3) */
 	nblock = dir->f_size / BLOCK_SIZE;
 
+	if (!(dir->f_mode & FMODE_X)){
+		return -E_PERM_DENY;
+	}
+
 	// Step 2: Iterate through all blocks in the directory.
 	for (int i = 0; i < nblock; i++) {
 		// Read the i'th block of 'dir' and get its address in 'blk' using 'file_get_block'.
@@ -675,6 +679,10 @@ int file_create(char *path, struct File **file) {
 		return r;
 	}
 
+	if (!(dir->f_mode & FMODE_X)){
+		return -E_PERM_DENY;
+	}
+
 	if (dir_alloc_file(dir, &f) < 0) {
 		return r;
 	}
@@ -794,6 +802,10 @@ int file_remove(char *path) {
 	// Step 1: find the file on the disk.
 	if ((r = walk_path(path, 0, &f, 0)) < 0) {
 		return r;
+	}
+
+	if (!(f->f_mode & FMODE_X)){
+		return -E_PERM_DENY;
 	}
 
 	// Step 2: truncate it's size to zero.
